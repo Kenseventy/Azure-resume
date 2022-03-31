3a9ae4f193912b227baed4da001cc7d538a9dde0
unknown <kmcomptech@gmail.com>
2022-03-31 15:35:23 -0500

    Updates to structure, Created Main.js with full function api

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,8 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     GetVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const localApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApi = 'https://getresumecounterkm.azurewebsites.net/api/GetResumeCounter?code=Wo7riS55yiaMrk/uqTqTIAFvABe/awaz5fjSQvf9Bukhe8C/E00yag==';
 
 const GetVisitCount = () => {
     let count = 30;
