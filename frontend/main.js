0df94c31c1b6928f1e68b361bf496829902fe2de
unknown <kmcomptech@gmail.com>
2022-12-22 14:10:27 -0600

    change back to old counter

--- frontend/main.js
+++ frontend/main.js
@@ -9,38 +9,16 @@ const functionApi = 'https://getresumecounterkm.azurewebsites.net/api/GetResumeC
 
 
 const GetVisitCount = () => {
-    // Check if the counter value exists in localStorage
-    if (localStorage.getItem('counter') === null) {
-      // If the counter value does not exist, initialize it to 1
-      localStorage.setItem('counter', 1);
-  
-      // Call the functionApi URL
-      let count = 30;
-      fetch(functionApi).then(response => {
-        return response.json()
-      }).then(response =>{
-        console.log("Website called function API.")
-        count = response.count;
-        document.getElementById('counter').innerText = count;
-      }).catch(function(error){
-        console.log(error)
-      });
-      return count;
-    } else {
-      // If the counter value exists, check if it was updated within the last hour
-      if (new Date().getTime() - localStorage.getItem('timestamp') > 3600000) {
-        // If the counter was not updated within the last hour, increment the counter value
-        localStorage.setItem('counter', parseInt(localStorage.getItem('counter')) + 1);
-      }
-  
-      // Get the current value of the counter
-      const counter = localStorage.getItem('counter');
-  
-      // Update the HTML element with the id "counter" with the current counter value
-      document.getElementById('counter').innerHTML = counter;
-    }
-  
-    // Update the timestamp when the counter was last updated
-    localStorage.setItem('timestamp', new Date().getTime());
-  }
-  
+  let count = 30;
+  fetch(functionApi).then(response => {
+      return response.json()
+  }).then(response =>{
+      console.log("Website called function API.")
+      count = response.count;
+      document.getElementById('counter').innerText = count;
+  }).catch(function(error){
+      console.log(error)
+  });
+  return count;
+
+}
