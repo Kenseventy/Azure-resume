a25fa4ae5a1cd4913bb649e1aa7a52d7c2e14caa
unknown <kmcomptech@gmail.com>
2023-01-25 22:51:44 -0600

    updated roles

--- frontend/main.js
+++ frontend/main.js
@@ -4,7 +4,6 @@ window.addEventListener('DOMContentLoaded', (event) =>{
 
 
 
-const localApi = 'http://localhost:7071/api/GetResumeCounter';
 const functionApi = 'https://getresumecounterkm.azurewebsites.net/api/GetResumeCounter?code=Wo7riS55yiaMrk/uqTqTIAFvABe/awaz5fjSQvf9Bukhe8C/E00yag==';
 
 
